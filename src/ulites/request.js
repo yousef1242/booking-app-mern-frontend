0a50a64f5ecb804663ecc4e4b4c0fb4a718b58c2
yousef <yousefahmedelshahed@gmail.com>
2023-06-07 23:40:29 +0300

    change link of backend

--- src/ulites/request.js
+++ src/ulites/request.js
@@ -3,7 +3,7 @@ import axios from "axios"
 
 
 const request = axios.create({
-    baseURL : "http://localhost:8080"
+    baseURL : "https://booking-app-mern-backend.onrender.com"
 })
 
 export default request
